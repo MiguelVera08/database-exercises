f49bec70a15b311c80f8e69d50449085e56c2da8
Miguel Vera <miguel.vera7908@gmail.com>
2020-03-02 12:47:59 -0600

    mysql update

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,15 +1,16 @@
 CREATE DATABASE IF NOT EXISTS codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
 USE codeup_test_db;
 
-CREATE TABLE albums
+CREATE TABLE IF NOT EXISTS albums
 (
 	id INT UNSIGNED auto_increment,
+    artist VARCHAR(100) not null,
 	name VARCHAR(100) not null,
-	artist VARCHAR(100) not null,
 	release_date INT(4) not null,
 	sales DECIMAL(10,2) not null,
 	genre VARCHAR(100) not null,
 	primary key (id)
 );
-
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,2 +1,25 @@
 USE codeup_test_db;
 
+SELECT * FROM albums;
+
+SELECT * FROM albums
+WHERE release_date < 1980;
+
+SELECT * FROM albums
+WHERE artist = 'Michael Jackson';
+
+# UPDATE albums
+# SET sales = (sales * 10);
+
+# UPDATE albums
+# SET release_date = REPLACE(release_date, 19, 18)
+# WHERE release_date < 1980;
+
+# UPDATE albums
+# SET artist = REPLACE(artist, 'Michael Jackson','Peter Jackson')
+# WHERE artist = 'Michael Jackson';
+
+
+
+
+
